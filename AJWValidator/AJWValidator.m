03c174d78a228a110eb5d1f375227b79fe840dc6
George Galai <galaipgeorge@gmail.com>
2020-09-23 13:20:26 +0300

    Update AJWValidator.m
    update email pattern

--- AJWValidator/AJWValidator.m
+++ AJWValidator/AJWValidator.m
@@ -52,7 +52,16 @@ const NSString * NSStringFromAJWValidatorState(AJWValidatorState state) {
     }
 }
 
-NSString * const AJWValidatorRegularExpressionPatternEmail = @"^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-+]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z‌​]{2,4})$";
+// The Official Standard: RFC 5322
+// http://www.regular-expressions.info/email.html
+
+NSString * const AJWValidatorRegularExpressionPatternEmail = @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
+@"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
+@"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
+@"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"
+@"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
+@"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
+@"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
 NSString * const AJWValidatorRegularExpressionPatternContainsNumber = @".*\\d.*";
 
